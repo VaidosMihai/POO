16468af91a3c7cd588033dd197ca67463c32c1a1
VaidosMihai <vaidos.mihaiiulian@gmail.com>
2020-11-17 15:08:38 +0200

    Update

--- POO/Client.h
+++ POO/Client.h
@@ -15,38 +15,39 @@ class Client {
 public:
     Client(std::string,int);
     void cumpara(Film& film);
-    void vizioneaza(Film& film);
+    void vizioneaza();
     friend std::ostream& operator<<(std::ostream& out, const Client& c);
 }; //CLASA CLIENT
 
-Client::Client(std::string NUME,int VARSTA) //CITIRE CLIENT
+Client::Client(std::string numele,int varst)
 {
-    this->nume_client = NUME;
-    this->varsta = VARSTA;
+    this->nume_client = numele;
+    this->varsta = varst;
     std::cout<<"\nClientul nostru "<<this->nume_client<<" are varsta de "<<this->varsta;
-}
+}//CITIRE CLIENT
 
-void Client::cumpara(Film& f)  //ADAUGA UN FILM IN COLECTIE
+void Client::cumpara(Film& f)
 {
     film.push_back(f);
-}
+}//ADAUGA UN FILM IN COLECTIE
 
-void Client::vizioneaza(Film& f) //VIZIONEAZA PRIMUL FILM DIN COLECTIE
+void Client::vizioneaza()
 {
     film.erase(film.begin());
-}
+}//VIZIONEAZA PRIMUL FILM DIN COLECTIE
 
-std::ostream& operator<<(std::ostream& out, const Client& c) //AFISEAZA COLECTIA DE FILME
+std::ostream& operator<<(std::ostream& out, const Client& c)
 {
     out << "\nColectia de filme a clientului contine :\n";
 
-    copy(begin(c.film), end(c.film) - 1,std::ostream_iterator<Filme>(out, " , "));
+    copy(begin(c.film), end(c.film) - 1,std::ostream_iterator<Film>(out, " , "));
+
     if (c.film.size() > 0)
     {
         out << c.film.at(c.film.size() - 1) << ' ';
     }
     out<<"\n";
     return out;
-}
+}//AFISEAZA COLECTIA DE FILME
 
 #endif //POO_CLIENT_H
--- POO/Filme.h
+++ POO/Filme.h
@@ -2,39 +2,38 @@
 // Created by mihai on 11/3/2020.
 //
 #include <iostream>
-#ifndef POO_FILM_H
-#define POO_FILM_H
-class Film {
+#ifndef POO_FILME_H
+#define POO_FILME_H
+class Film{
     std::string nume;
     int varsta_necesara;
     float pret;
     int stoc;
 
 public:
-    Film();
-    Film(std::string theName, int varsta_necesara,float pret,int stoc);
+    Film(std::string numele, int varsta, float pret, int stocul);
     std::string get_name() const;
     std::string informatii();
     bool in_stoc();
-    void citire(std::string nume,int varsta,float pret,int stoc);
-    friend std::ostream& operator<<(std::ostream& ostr, const Filme& stud);
+    void citire(std::string numele,int varsta,float pret,int stoc);
+    friend std::ostream& operator<<(std::ostream& ostr, const Film& stud);
 }; //CLASA FILME
 
 //========FUNCTII============
 
-std::ostream& operator<<(std::ostream& ostr, const Filme& stud);
+std::ostream& operator<<(std::ostream& out, const Film& f);
 
-Film::Film(std::string nume, int varsta,float pret,int stoc)
+Film::Film(std::string numele, int varsta, float pretul, int stocul)
 {
-    citire(nume,varsta,pret,stoc);
+    citire(numele,varsta,pretul,stocul);
 }
 
-void Film::citire(std::string nume,int varsta,float pret,int stoc)
+void Film::citire(std::string numele,int varsta,float pretul,int stocul)
 {
-    nume=nume;
+    nume=numele;
     varsta_necesara = varsta;
-    pret=pret;
-    stoc=stoc;
+    pret=pretul;
+    stoc=stocul;
     std::cout<<"\nNume film: "<<this->nume<<"\nVarsta necesara: "<<this->varsta_necesara<<"\nPret: "<<this->pret<<"\nStoc: "<<this->stoc<<"\n";
 }//CITIRE FILME
 
@@ -43,26 +42,27 @@ std::string Film::get_name() const
     return nume;
 }
 
-std::ostream& operator<<(std::ostream& out, const Filme& f)
-{
-    out << f.get_name();
-    return out;
-}//AFISARE
-
-std::string Film::informatii()
+std::string Film:: informatii()
 {
     if(varsta_necesara == 18) return "Acest film este dedicat strict pentru adulti!\n";
     else if(varsta_necesara >= 14) return "Acest film poate fi vizionat numai cu acordul parintilor!\n";
     else return "Acest film este pentru copii!!\n";
 }//CATEGORII DE VARSTA
 
-bool Film::in_stoc()
+bool Film:: in_stoc()
 {
     return stoc > 0;
 }//DISPONIBILITATE
 
-std::ostream& operator<<(std::ostream& out,Filme& f)
+std::ostream& operator<<(std::ostream& out, const Film& f)
 {
+    out << f.get_name();
+    return out;
+}//AFISARE PENTRU VECTORUL DE FILME
+
+std::ostream& operator<<(std::ostream& out,Film& f)
+{
+    out << f.get_name();
     if(f.in_stoc())out<<"\nDISPONIBIL\n";
     else out<<"\nSTOC EPUIZAT\n";
     out<<f.informatii();
@@ -70,4 +70,4 @@ std::ostream& operator<<(std::ostream& out,Filme& f)
     return out;
 }//AFISARE STOC SI CATEGORIE VARSTA
 
-#endif //POO_FILM_H
+#endif //POO_FILME_H
