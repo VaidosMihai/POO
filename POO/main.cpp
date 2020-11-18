8bd91074950052d9b80d842ceaa5b6241a74c2ae
VaidosMihai <63729875+VaidosMihai@users.noreply.github.com>
2020-11-18 12:25:27 +0200

    Update main.cpp

--- POO/main.cpp
+++ POO/main.cpp
@@ -7,31 +7,36 @@
 int main()
 {
     std::cout<<"Angajatii magazinului:";
-    Angajat A("Grigore",1,1);
-    Angajat B("Andreea",2,1);
-    Angajat C("Jon",3,2);
+    Angajat A("Grigore",1);
+    Angajat B("Andreea",2);
+    Angajat C("Jon",3);
 
     std::cout<<"\n";
 
-    Film film1 ("Cars", 12 ,18.55,12);
+    Film film1 ("Cars", 8 ,18.55,12);
     std::cout<<film1;
     Film film2 ("X-MAN", 18,15.55,0);
     std::cout<<film2;
-    Film film3 ("WALL-E",14,20,5);
+    Film film3 ("WALL-E",10,20,5);
     std::cout<<film3;
     Film film4 ("Vampire",18,10.55,23);
     std::cout<<film4;
 
     Client client1("George",18,100);
-
-    client1.cumpara(film1);
+    client1.cumpara(film1,A);
     A.adauga_caserie(film1);
-    client1.cumpara(film2);
-    client1.cumpara(film3);
-    client1.cumpara(film4);
-    std::cout << client1 << std::endl; // afiseaza colectie filme client curenta
+    client1.cumpara(film2,A);
+    client1.cumpara(film3,A);
+    client1.cumpara(film4,A);
     client1.vizioneaza(film3);
+    std::cout << client1 << std::endl; // afiseaza colectie filme client
 
+    Client client2("Aurel",10,20);
+    client2.cumpara(film3,B);
+    client2.cumpara(film2,B);
+    std::cout<<client2<<std::endl;
+
+    std::cout<<"MAGAZINUL SE INCHIDE\n"<<A<<B;
 
     return 0;
 }
