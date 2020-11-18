7762dc1294de09583cd2e521069606adfbae9ec3
VaidosMihai <vaidos.mihaiiulian@gmail.com>
2020-11-18 12:22:25 +0200

    Update 0.0.1

--- POO/Angajat.h
+++ POO/Angajat.h
@@ -8,37 +8,35 @@
 class Angajat{
     std::string nume_angajat;
     int id;
-    int schimb;
-    int bani_caserie;
+    float bani_caserie;
 public:
     void adauga_caserie(Film& f);
-    int get_bani() const;
-    Angajat(std::string,int,int,int);
+    Angajat(std::string,int,float);
     Angajat(Angajat&);
+    friend std::ostream& operator<<(std::ostream& out, const Angajat& a);
 };
 
 //========FUNCTII============
-int Angajat::get_bani() const
+std::ostream& operator<<(std::ostream& out, const Angajat& a)
 {
-    return bani_caserie;
+    out<<a.nume_angajat<<" a facut vanzari in total de "<<a.bani_caserie<<" de lei.\n";
+    return out;
 }
 void Angajat::adauga_caserie(Film& f)
 {
     bani_caserie=bani_caserie+f.get_pret();
 }
-Angajat::Angajat(std::string NUME,int ID,int SCHIMB,int bani_caserie=0)
+Angajat::Angajat(std::string NUME,int ID,float bani=0)
 {
     this->nume_angajat=NUME;
     this->id=ID;
-    this->schimb=SCHIMB;
-    std::cout<<"\nNume: "<<this->nume_angajat<<"\nid: "<<this->id<<"\nSchimbul: "<<this->schimb;
+    this->bani_caserie=bani;
+    std::cout<<"\nNume: "<<this->nume_angajat<<"\nid: "<<this->id;
 }
 Angajat::Angajat(Angajat& X)
 {
     this->nume_angajat=X.nume_angajat;
     this->id=X.id;
-    this->schimb=X.schimb;
-    std::cout<<this->nume_angajat<<" "<<this->id<<" "<<this->schimb;
+    std::cout<<this->nume_angajat<<" "<<this->id;
 }
-
 #endif //POO_ANGAJAT_H
