718385ac5ee97e3851a38caaa783b01cb8d309a8
VaidosMihai <vaidos.mihaiiulian@gmail.com>
2020-12-11 20:24:39 +0200

    Update 0.0.5

--- POO/main.cpp
+++ POO/main.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
-#include "Filme.h"
-#include "Angajat.h"
-#include "Client.h"
+#include "Headers/Film.h"
+#include "Headers/Angajat.h"
+#include "Headers/Client.h"
+#include "Headers/Serial.h"
 
 
 int main()
 {
     std::cout<<"Angajatii magazinului:";
-    Angajat A("Grigore",1);
-    Angajat B("Andreea",2);
-    Angajat C("Jon",3);
+    Angajat A("Grigore","Filme Actiune",0);
+    Angajat B("Andreea","Filme SF",0);
+    Angajat C("Jon","Filme Under 18",0);
 
     std::cout<<"\n";
 
-    Film film1 ("Cars", 8 ,18.55,12);
-    std::cout<<film1;
-    Film film2 ("X-MAN", 18,15.55,0);
-    std::cout<<film2;
-    Film film3 ("WALL-E",10,20,5);
-    std::cout<<film3;
-    Film film4 ("Vampire",18,10.55,23);
-    std::cout<<film4;
-
+    Film f1 ("Cars", 8 ,18.55,12);
+    std::cout<<f1;
+    Film f2 ("X-MAN", 18,15.55,0);
+    std::cout<<f2;
+    Film f3 ("WALL-E",14,20,5);
+    std::cout<<f3;
+    Film f4 ("Vampire",18,10.55,23);
+    std::cout<<f4;
+    Serial s1("The 100", 18, 10.99, 15, 1,24);
+    std::cout<<s1;
     Client client1("George",18,100);
-    client1.cumpara(film1,A);
-    A.adauga_caserie(film1);
-    client1.cumpara(film2,A);
-    client1.cumpara(film3,A);
-    client1.cumpara(film4,A);
-    client1.vizioneaza(film3);
+    client1.cumpara(f1,A);
+    client1.cumpara(f2,A);
+    client1.cumpara(f3,A);
+    client1.cumpara(f4,A);
+    client1.cumpara(s1,A);
+    client1.vizioneaza(f3);
     std::cout << client1 << std::endl; // afiseaza colectie filme client
 
     Client client2("Aurel",10,20);
-    client2.cumpara(film3,B);
-    client2.cumpara(film2,B);
+    client2.cumpara(f3,B);
+    client2.cumpara(f1,B);
+    client2.cumpara(s1,B);
     std::cout<<client2<<std::endl;
 
-    std::cout<<"MAGAZINUL SE INCHIDE\n"<<A<<B;
+    std::cout<<A<<B;
 
-    return 0;
 }
