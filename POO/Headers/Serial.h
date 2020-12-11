343ef50b7c0eaef6c8021a30cb77f3f0bb63340e
VaidosMihai <vaidos.mihaiiulian@gmail.com>
2020-12-11 21:14:08 +0200

    Update 0.0.7

--- POO/Headers/Serial.h
+++ POO/Headers/Serial.h
@@ -13,7 +13,7 @@ class Serial:public Film
     int sezon;
     int episoade;
 public:
-
+    virtual ~Serial() = default;
     Serial(std::string numele, int varsta, float pret, int stocul, int sez, int episoade);
     std::string informatii()const override;
 };
