76165352168c467632f556b400dd2e69579a95f1
VaidosMihai <63729875+VaidosMihai@users.noreply.github.com>
2020-12-11 20:32:55 +0200

    Add files via upload

---
+++ POO/CPP/Film.cpp
@@ -0,0 +1,61 @@
+//
+// Created by mihai on 12/1/2020.
+//
+
+#include "../Headers/Film.h"
+
+Film &Film::operator=(const Film &f) {
+    this->nume = f.nume;
+    this->varsta_necesara = f.varsta_necesara;
+    this->pret = f.pret;
+    this->stoc=f.stoc;
+    return (*this);
+} //OPERATOR ( = )
+
+Film::Film(std::string numele, int varsta, float pretul, int stocul)
+{
+    nume=numele;
+    varsta_necesara = varsta;
+    pret=pretul;
+    stoc=stocul;
+    std::cout<<"\nNume: "<<this->nume<<"\nVarsta necesara: "<<this->varsta_necesara<<"\nPret: "<<this->pret<<"\nStoc: "<<this->stoc;
+}
+
+int Film::varst_necesara() const {
+    return varsta_necesara;
+} //RETURNEAZA varsta_necesara pentru un film
+
+double Film::get_pret()
+{
+    return pret;
+} //RETURNEAZA PRET FILM
+
+std::string Film::get_name() const
+{
+    return nume;
+}// RETURNEAZA NUMELE FILMULUI
+
+std::ostream& operator<<(std::ostream& out, const Film& f)
+{
+        if(f.in_stoc())out<<"\nDISPONIBIL\n";
+        else out<<"\nSTOC EPUIZAT\n";
+        out<<f.informatii();
+        out<<"=====================================================";
+        return out;
+    //AFISARE STOC SI CATEGORIE VARSTA
+}
+
+
+std::string Film:: informatii()const
+{
+    if(varsta_necesara == 18) return "Acest film este dedicat strict pentru adulti!\n";
+    else if(varsta_necesara >= 14) return "Acest film poate fi vizionat numai cu acordul parintilor!\n";
+    else return "Acest film este pentru copii!!\n";
+}//CATEGORII DE VARSTA
+
+bool Film:: in_stoc()const
+{
+    return stoc > 0;
+}//DISPONIBILITATE
+
+
